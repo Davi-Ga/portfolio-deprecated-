ed8c607bc7e6aa2f0bfc1351075d6ae022cd7d31
Davi-Ga <davigaldinoky@gmail.com>
2022-04-13 23:05:45 -0300

    Ajustes no app

--- src/app.py
+++ src/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__,static_url_path='',static_folder='static', template_folder=
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route("/", methods=['GET'])
+@app.route("/",methods=['GET'])
 def index_get():
     return render_template("index.html")
+
 
