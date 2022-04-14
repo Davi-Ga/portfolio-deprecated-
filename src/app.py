02a0ab40cd1994824af334fbaa4a341195857123
Davi-Ga <davigaldinoky@gmail.com>
2022-04-13 21:21:14 -0300

    Adicionado um if para execução de um único programa

--- src/app.py
+++ src/app.py
@@ -1,14 +1,13 @@
 # Further config access https://www.digitalocean.com/community/tutorials/how-to-structure-large-flask-applications
 
-from flask import Flask, render_template, jsonify, make_response
+from flask import Flask, render_template, jsonify, make_response,redirect
 
 app = Flask(__name__,static_url_path='',static_folder='static', template_folder='templates')
 
+if __name__ == '__main__':
+    app.run()
 
 @app.route("/", methods=['GET'])
 def index_get():
     return render_template("index.html")
 
-@app.route("/hello", methods=['GET'])
-def hello_get():
-    return make_response(jsonify({"hello": "world"}))
