10fd4f7269ee9a9b97b6f4b4d7bcc07364f5e482
Davi-Ga <davigaldinoky@gmail.com>
2022-07-19 12:50:54 -0300

    app.run altered

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 # Further config access https://www.digitalocean.com/community/tutorials/how-to-structure-large-flask-applications
 
 from flask import Flask, render_template, jsonify, make_response,redirect
+import os
 
 app = Flask(__name__,static_url_path='',static_folder='static', template_folder='templates')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port=int(os.environ.get('PORT', 8080))
+    app.run(debug=True,host='0.0.0.0',port=port)
 
 @app.route("/",methods=['GET'])
 def index_get():
