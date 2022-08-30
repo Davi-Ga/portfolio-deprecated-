c6d1afc303f8ea8905f55d138491f47fc7ac82b2
Davi-Ga <davigaldinoky@gmail.com>
2022-08-30 13:38:20 -0300

    app for fly

--- src/app.py
+++ src/app.py
@@ -6,8 +6,7 @@ import os
 app = Flask(__name__,static_url_path='',static_folder='static', template_folder='templates')
 
 if __name__ == '__main__':
-    port=int(os.environ.get('PORT', 8080))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(debug=True)
 
 @app.route("/",methods=['GET'])
 def index_get():
