bbe09514a0e7156a85f9722d5a4324f311606ccd
Davi-Ga <davigaldinoky@gmail.com>
2022-09-27 00:40:04 -0300

    funções para abrir e fechar menu com callback e funções ternárias

--- src/static/script.js
+++ src/static/script.js
@@ -1,1 +1,11 @@
-(() => {})();
+// Abre e fecha menu mobile
+
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
+
+menuMobile.addEventListener('click',() =>{ //função callback
+    menuMobile.classList.toggle('bi-list')
+    ? menuMobile.classList.replace('bi-list','bi-x') //IF ternário
+    : menuMobile.classList.replace('bi-x','bi-list');
+    body.classList.toggle('menu-nav-active');
+});
