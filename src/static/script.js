fd892e5bc06eef65110dc220f2fd6b19ac8b50b8
Davi-Ga <davigaldinoky@gmail.com>
2022-09-27 00:44:10 -0300

    contains adicionado

--- src/static/script.js
+++ src/static/script.js
@@ -4,7 +4,7 @@ const menuMobile = document.querySelector('.menu-mobile');
 const body = document.querySelector('body');
 
 menuMobile.addEventListener('click',() =>{ //função callback
-    menuMobile.classList.toggle('bi-list')
+    menuMobile.classList.contains('bi-list')
     ? menuMobile.classList.replace('bi-list','bi-x') //IF ternário
     : menuMobile.classList.replace('bi-x','bi-list');
     body.classList.toggle('menu-nav-active');
